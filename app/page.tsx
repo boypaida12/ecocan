fcf81a5ac6aa28441b0937b94c094c9e5c62e42f
boypaida12 <baddysackey5@gmail.com>
2024-07-09 01:09:34 +0000

    WIP: setup assets

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Ecocan</Button>
+      <Image src="/assets/images/google-play.png" width={230.4} height={34} alt="download"/>
+      <Image src="/assets/images/apple-store.png" width={230.4} height={34} alt="download"/>
+      <Image src="/assets/images/ecocan-app.svg" width={491} height={676} alt="download"/>
+      <Image src="/assets/images/ecocan-logo.svg" width={46} height={46} alt="download"/>
     </main>
   );
 }
