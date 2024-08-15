7839ce369023f474d8346afae321ea986ea8813f
boypaida12 <baddysackey5@gmail.com>
2024-08-15 09:47:53 +0000

    WIP: adjust reusable components

---
+++ components/shared/image-and-item/image-and-item.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import clsx from "clsx";
+
+interface ImageAndItemProps {
+  title?: string;
+  subtitle?: string;
+  description?:string;
+  item?: React.ReactNode;
+  image?: React.ReactNode;
+  className: string;
+}
+
+export default function ImageAndItem({
+  title,
+  subtitle,
+  description,
+  item,
+  className,
+  image
+}: ImageAndItemProps) {
+  return (
+    <div className="max-w-[77.5rem] mx-auto">
+      <div className="text-center mb-12">
+        <h2 className="text-[2rem] font-bold text-[#23262FCC]">{title}</h2>
+        <p>{description}</p>
+        <p className="text-gray-600">{subtitle}</p>
+      </div>
+      <div className={clsx("flex flex-col md:flex-row gap-12", className)}>
+        <div className="md:flex-0 space-y-4">
+          {image}
+        </div>
+        <div className="md:w-1/2 space-y-4">
+          {item}
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/shared/text-with-cards/text-with-cards.tsx
+++ components/shared/text-with-cards/text-with-cards.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import CustomCard from './custom-card'
+import React from "react";
+import CustomCard from "./custom-card";
+import Link from "next/link";
 
 interface TextWithCardsProps {
-    title?: string,
-    description?: string,
-    customCard?: React.ReactNode
+  title?: string;
+  description?: React.ReactNode;
+  customCard?: React.ReactNode;
 }
 
-export default function TextWithCards({title = "Get Started", description = "Whether you want to keep your environment clean, earn extra cash, or to safeguard your health from fake drinks. With our free-to-use EcocanApp, it's all just a click away!", customCard = <CustomCard/>}:TextWithCardsProps) {
+export default function TextWithCards({
+  title = "Get Started",
+  description = (
+    <p>
+      Whether you want to keep your environment clean, earn extra cash, or to
+      safeguard your health from fake drinks. With our <span className="font-semibold">free-to-use</span> EcocanApp,
+      it&apos;s all just a <Link href="/" className="text-primary">click away!</Link>
+    </p>
+  ),
+  customCard = <CustomCard />,
+}: TextWithCardsProps) {
   return (
-    <div className='max-w-[77.5rem] mx-auto py-[6.25rem]'>
-        <div className="text-accent w-[72%]">
-            <h2 className='font-bold text-[2.5rem]'>{title}</h2>
-            <p>{description}</p>
-        </div>
-        {customCard}
+    <div className="max-w-[77.5rem] mx-auto">
+      <div className="text-accent w-[72%]">
+        <h2 className="font-bold text-[2.5rem]">{title}</h2>
+        {description}
+      </div>
+      {customCard}
     </div>
-  )
+  );
 }
