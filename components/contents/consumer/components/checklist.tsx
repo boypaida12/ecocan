7774e4878c34463355eebc67c2011f5095401969
Bernard <bernardsackey95@gmail.com>
2024-08-30 12:04:07 +0000

    WIP: recycler content

--- components/contents/consumer/components/checklist.tsx
+++ components/contents/consumer/components/checklist.tsx
@@ -1,38 +1,36 @@
-import { LucideCheckCircle2 } from "lucide-react";
 import React from "react";
+import { LucideCheckCircle2 } from "lucide-react";
 
-const steps = [
-  {
-    id: 1,
-    step: "Verify empties' eligibility by scanning attached ECOCAN security codes. DO NOT rely on your naked eye",
-  },
-  {
-    id: 2,
-    step: "Use ECOCAN Map to navigate to the closest or your preferred ECO-station",
-  },
-  {
-    id: 3,
-    step: "Hand-over your intact empties to ECO-Station Egents, who will verify them, and digitally issue applicable deposit into your ECO-Wallet",
-  },
-  {
-    id: 4,
-    step: "Alternatively, feed eligible empties into ECOCAN R.V.M, which will verify the package and transfer applicable deposit into your ECO-Wallet",
-  },
-  {
-    id: 5,
-    step: "That's all. You can then spend your money as you please",
-  },
-];
-
-export default function CheckList() {
-  return steps.map((step) => {
-    return (
-      <div className="flex items-center gap-3 mt-5" key={step.id}>
-        <div>
-            <LucideCheckCircle2 className="fill-primary text-white" />
-        </div>
-        <p className="text-accent text-sm">{step.step}</p>
-      </div>
-    );
-  });
+interface CheckListItem {
+  id: number | string;
+  step?: React.ReactNode;
+  description?: React.ReactNode;
 }
+
+interface CheckListProps {
+  items: CheckListItem[];
+  className?: string;
+}
+
+const CheckList: React.FC<CheckListProps> = ({ 
+  items, 
+  className, 
+}) => {
+  return (
+    <div className={className}>
+      {items.map((item) => (
+        <div className="flex items-start gap-3 mt-5" key={item.id}>
+          <div>
+            <LucideCheckCircle2 className="fill-primary text-white" />
+          </div>
+          <div>
+            <div>{item.step}</div>
+            <div className="text-accent text-sm">{item.description}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CheckList;
