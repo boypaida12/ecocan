09d16177db10d55ee6dc92afbd0ffd1622c0a516
Francois Mavunila <mavunila002@gmail.com>
2024-09-23 03:58:32 +0200

    finished ecoRecycler hero

--- components/shared/heros/recycler-hero.tsx
+++ components/shared/heros/recycler-hero.tsx
@@ -36,18 +36,18 @@ export default function RecyclerHero() {
       <div className="lg:h-[36rem] xl:h-[47.575rem]">
         <div className="bg-[url('/assets/images/recycler/recycler-bg.png')] lg:h-[36rem] xl:h-[44rem] bg-cover relative after:absolute after:inset-0 after:content-[''] after:bg-black/90 after:opacity-50 after:z-10">
           <div className="xl:max-w-[65rem] flex flex-col md:flex md:flex-row mx-auto lg:my-0 lg:gap-12">
-            <div className="mt-60 px-4 xl:px-0 text-white z-50 mx-auto">
+            <div className="mt-60 px-4 xl:px-0 text-white z-50 mx-auto w-[70.625rem] text-left">
               <div>
-                <h1 className="lg:text-6xl text-2xl xl:text-[4rem] text-center font-semibold text-white">
-                  Highest Quality{" "}
+                <h1 className="lg:text-6xl text-2xl xl:text-[4rem] text-left font-semibold text-white">
+                  Highest Quality<br/>
                   <span className="bg-gradient-to-r from-[#228B22] via-[#4AC63F] to-[#FFDD4C] text-transparent bg-clip-text">
                     Recyclate
                   </span>
                 </h1>
               </div>
               <div className="tracking-wide text-sm lg:text-base xl:text-lg font-light text-white my-10 space-y-4">
-                <p className="text-center">
-                  Cut your recyclate processing loss rate, with
+                <p className="text-left">
+                  Cut your recyclate processing loss rate, with<br/>
                   segregated-at-source empties
                 </p>
               </div>
